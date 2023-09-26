379569cbd087f9ad5ee2efd3915b3c0ab00145b2
francisdoble <francis.doble@atram.com.ph>
2023-09-26 22:19:51 +0800

    Pushed API functions

--- yt_api.py
+++ yt_api.py
@@ -1,14 +1,15 @@
 import os
+from dotenv import load_dotenv
 
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
 from googleapiclient.discovery import build
 import requests
 from youtube_transcript_api import YouTubeTranscriptApi
 import pandas as pd
+
 class youtubeAPI():
-    api_key = '' #TODO: paste your API key here; https://console.cloud.google.com/apis/dashboard
+    load_dotenv()
+    api_key = os.environ.get("API_KEY")
+
     def __init__(self):
         self.youtube = build('youtube', 'v3', developerKey= self.api_key)
 
@@ -30,26 +31,32 @@ class youtubeAPI():
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
 
-
     # Returns a JSON list of youtube videos from the search query
     # Parameters:
     #   search_query - string of keywords of videos that will be searched
-    #   result_count - the number of videos you want to retrieve from the search
-    def search_vids(self, search_query, result_count):
+    #   max_results - the number of videos you want to retrieve from the search
+    def search_vids(self, search_query, max_results):
         request = self.youtube.search().list(
             part="snippet",
-            maxResults=result_count,
+            maxResults=max_results,
             q=search_query
         )
         response = request.execute()
         response = response['items']
-        print("--- Search Results ---\n")
         return response
-
-    # Returns comment threads from a video
+    
+    # Returns the video details and statistics of a single video ID
+    def get_vid_details(self,video_id):
+        statistics = self.youtube.videos().list(
+            part='snippet,statistics',
+            id=video_id
+        ).execute()
+        return statistics
+    
+    # Returns comment threads from a single video
     # Parameters
     #   id - the videoId of the video you want to get the comments from
-    def comments_from_vidId(self,id):
+    def get_comments_from_vidId(self,id):
         video_response=self.youtube.commentThreads().list(
 			part='snippet,replies',
 			videoId=id
@@ -67,12 +74,7 @@ class youtubeAPI():
                     replycount = item['snippet']['totalReplyCount']
                     file.write(f"Author: {comment_author_name} - {comment_author_id}\n")
                     file.write(f"{comment} - - - Reply Count: {replycount}\n")
-                    data = {
-                        'author':comment_author_name,
-                        'authorId':comment_author_id,
-                        'comment':comment
-                    }
-                    data_list.append(data)
+                    
                     replies = []
                     # if reply is there
                     if replycount>0:
@@ -87,13 +89,16 @@ class youtubeAPI():
                             replies.append(reply)
                             file.write(f"{reply}\n")
                     file.write("\n------------------\n")
-        
-                    # print comment with list of reply
-                    print(comment, replies, end = '\n\n')
-
+                    data = {
+                        'author':comment_author_name,
+                        'authorId':comment_author_id,
+                        'comment':comment,
+                        'replies':replies,
+                        'videoId':id
+                    }
+                    data_list.append(data)
                     # empty reply list
                     replies = []
-        
                 # Again repeat
                 if 'nextPageToken' in video_response:
                     video_response = self.youtube.commentThreads().list(
@@ -103,20 +108,39 @@ class youtubeAPI():
                         ).execute()
                 else:
                     break
-
         return data_list
+    
+    #Get comments from an array of video Ids
+    def get_comments_from_videos(self,vidIds):
+        comments_list = []
+        for vid in vidIds:
+            vid_comment = self.comments_from_vidId(vid)
+            comments_list.append(vid_comment)
+        comments_list = [item for sublist in comments_list for item in sublist]
+        return comments_list
 
     def get_transcript(self,video_id):
         return YouTubeTranscriptApi.get_transcript(video_id)
     
-    def get_vid_statistics(self,video_id):
-        statistics = self.youtube.videos().list(
-            part='snippet,statistics',
-            id=video_id
-        ).execute()
-        print(statistics)
-        return statistics
+    # Get comments from a video search query; Returns a JSON list of comments
+    def get_comments_from_search(self,search_query,max_result):
+        search_results = self.search_vids(search_query,max_result)
+        vid_ids = []
+        for res in search_results:
+            id = res['id']['videoId']
+            vid_ids.append(id)
+        return self.get_comments_from_videos(vid_ids)
 
+    # Get video details and statistics of videos from a search query; Returns a JSON list of video details and statistics
+    def get_vid_details_from_search(self,search_query,max_result):
+        videos = self.search_vids(search_query,max_result)
+        vid_details = []
+        for vid in videos:
+            id = vid['id']['videoId']
+            vid_details.append(self.get_vid_details(id))
+        return vid_details
+    
+    # Accepts a JSON list of video details and statistics then converts video details to dataframe
     def video_stats_to_dataframe(self,videos):
         data_list = []
         print(videos)
@@ -143,35 +167,5 @@ class youtubeAPI():
                 }
 
                 data_list.append(data)
-
         df = pd.DataFrame(data_list)
         return df
-    
-    def get_comments_fromVids(self,vidIds):
-        comments_list = []
-        for vid in vidIds:
-            vid_comment = self.comments_from_vidId(vid)
-            comments_list.append(vid_comment)
-        return comments_list
-    
-    def get_comments_from_search(self,search_query,max_result):
-        search_results = self.search_vids(search_query,max_result)
-        vid_ids = []
-        for res in search_results:
-            id = res['id']['videoId']
-            vid_ids.append(id)
-        return self.get_comments_fromVids(vid_ids)
-
-def main():
-    youtube = youtubeAPI()
-    df = pd.DataFrame(youtube.get_comments_from_search("foreigner reacts to filipinos", 25))
-    df.to_csv('comments.csv',index=False)
-    
-
-    #vid_details = youtube.get_vid_statistics('CEc6TTW1Cpo')
-    #youtube.video_info_to_dataframe(vid_details).to_csv('vid_deets.csv',index=False)
-    #TODO: try the API functions here
-
-if __name__ == "__main__":
-    main()
-
