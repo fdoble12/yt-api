cfee4b5c0fd0881ac069051f806e7cb0e27ff93c
francisdoble <francis_doble@dlsu.edu.ph>
2023-12-26 13:18:36 +0800

    update

---
+++ data_collector.py
@@ -0,0 +1,16 @@
+import yt_api
+from datetime import datetime, timedelta
+import pandas as pd
+
+class Cl():
+
+    def __init__(self,keyNum):
+        self.keyNum = keyNum
+        self.api =yt_api.youtubeAPI(keyNum)
+    
+    def collect(self,search_query,max_results,published_after,published_before):
+        response = self.api.get_vid_details_from_search(search_query, max_results, published_after, published_before)
+        temp = self.api.video_stats_to_dataframe(response)
+        return temp
+    
+
